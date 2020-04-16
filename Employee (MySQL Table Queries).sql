55864bfdb57b4ee75333efc5de46f44afc73932e
Archit Kawale <55445578+Architkawale18@users.noreply.github.com>
2020-04-16 16:31:21 +0530

    Update and rename Employee (MySQL Table Queries.sql to Employee (MySQL Table Queries).sql

--- Employee (MySQL Table Queries).sql
+++ Employee (MySQL Table Queries).sql
@@ -1,6 +1,6 @@
 create table employee
 (
-employee_code bigint(50) primary key not null,
+employee_code bigint(50) primary key not null auto_increment,
 e_firstname varchar(25) not null,
 e_lastname varchar(25) not null,
 e_address varchar(100) not null,
